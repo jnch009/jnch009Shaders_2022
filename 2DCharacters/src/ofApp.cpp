8d6f3bad353fb4a58efefda67e907779d042ef54
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2022-09-23 11:30:30 -0700

    Naive approach of moving character

--- 2DCharacters/src/ofApp.cpp
+++ 2DCharacters/src/ofApp.cpp
@@ -73,7 +73,11 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	int numVerts = charMesh.getNumVertices();
+	for (int i = 0; i < numVerts; ++i)
+	{
+		charMesh.setVertex(i, charMesh.getVertex(i) + glm::vec3(0.2f, 0.0f, 0.0f));
+	}
 }
 
 //--------------------------------------------------------------
